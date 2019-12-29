dfd534be2dc9c49fa274740a0f691ad4ca12130c
tbrigljev <teo.brigljevic@bluewin.ch>
2019-12-29 15:21:19 +0100

    Actuasim

---
+++ KNX_basics/actuasim_iot/actuasim/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Adrien Lescourt"
+__copyright__ = "HES-SO 2015, Project EMG4B"
+__credits__ = ["Adrien Lescourt"]
+__version__ = "1.0.2"
+__email__ = "adrien.lescourt@gmail.com"
+__status__ = "Prototype"
